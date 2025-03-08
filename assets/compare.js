3734c2ebf39752d1bcf0a6a298f56f2084ff7c0d
MahdiBeyromi <mmbeyromi10382@gmail.com>
2025-03-08 12:27:16 +0330

    ajax : fix bug js and ajax code

--- assets/compare.js
+++ assets/compare.js
@@ -1,5 +1,7 @@
 jQuery(document).ready(function($) {
-    $('.wpc-product-compare-button').on('click', function() {
+    // Add to compare button click event
+    $('.wpc-product-compare-button').on('click', function(e) {
+        e.preventDefault();
         var productId = $(this).data('product-id');
         $('body').append('<div id="loading-overlay"><div id="loading-spinner"></div></div>');
 
@@ -17,28 +19,33 @@ jQuery(document).ready(function($) {
                     $('#wpc-compare-overlay').addClass('show');
                     $('#loading-overlay').remove();
                 } else {
-                    alert('Failed: ' + response.data);
+                    alert('خطا: ' + response.data);
                     $('#loading-overlay').remove();
                 }
             },
             error: function() {
-                alert('AJAX request failed!');
+                alert('درخواست AJAX با مشکل مواجه شد!');
                 $('#loading-overlay').remove();
             }
         });
     });
 
-    $('#wpc-go-to-compare').on('click', function() {
+    // Go to compare page
+    $('#wpc-go-to-compare').on('click', function(e) {
+        e.preventDefault();
         window.location.href = woocommerce_compare.compare_page_url;
     });
 
-    $('#wpc-dismiss-notification').on('click', function() {
+    // Dismiss notification
+    $('#wpc-dismiss-notification').on('click', function(e) {
+        e.preventDefault();
         $('#wpc-compare-notification').removeClass('show');
         $('#wpc-compare-overlay').removeClass('show');
-        
     });
 
-    $('.wpc-remove-product').on('click', function() {
+    // Remove product from compare
+    $('.wpc-remove-product').on('click', function(e) {
+        e.preventDefault();
         var productId = $(this).data('product-id');
         $('body').append('<div id="loading-overlay"><div id="loading-spinner"></div></div>');
 
@@ -52,31 +59,28 @@ jQuery(document).ready(function($) {
             },
             success: function(response) {
                 if (response.success) {
-                    // Remove the product from the compare list on the page immediately
                     $('.compare-item[data-product-id="' + productId + '"]').remove();
                     $('#loading-overlay').remove();
-                    
-                    // If no products are left, show a message
                     if ($('.compare-grid .compare-item').length === 0) {
-                        $('.compare-grid').html('<p>No products in compare list.</p>');
+                        $('.compare-grid').html('<p>هیچ کالایی برای مقایسه موجود نیست.</p>');
                     }
                 } else {
-                    alert('Failed: ' + response.data);
+                    alert('خطا: ' + response.data);
                     $('#loading-overlay').remove();
                 }
             },
             error: function() {
-                alert('AJAX request failed!');
+                alert('درخواست AJAX با مشکل مواجه شد!');
                 $('#loading-overlay').remove();
             }
         });
     });
 
+    // Add to cart button
     $('.add-to-cart-button').on('click', function(e) {
         e.preventDefault();
 
         var productId = $(this).closest('form').find('input[name="add-to-cart"]').val();
-
         $('body').append('<div id="loading-overlay"><div id="loading-spinner"></div></div>');
 
         $.ajax({
@@ -89,18 +93,17 @@ jQuery(document).ready(function($) {
             },
             success: function(response) {
                 if (response.success) {
-                    // Optionally, you can update the cart count or other elements
                     setTimeout(function() {
                         location.reload();
                     }, 100);
                 } else {
-                    alert('Failed to add to cart.');
+                    alert('افزودن به سبد خرید با مشکل مواجه شد.');
                     $('#loading-overlay').remove();
                 }
             },
             error: function() {
-                alert('AJAX request failed!');
-                $('#loading-overlay').remove(); 
+                alert('درخواست AJAX با مشکل مواجه شد!');
+                $('#loading-overlay').remove();
             }
         });
     });
--- includes/compare-functions.php
+++ includes/compare-functions.php
@@ -1,17 +1,24 @@
 <?php
 function wpc_add_to_compare() {
     check_ajax_referer('wpc_compare_nonce', 'nonce');
-
+    
     if (!isset($_POST['product_id'])) {
-        wp_send_json_error('Product ID is missing');
+        wp_send_json_error(array('message' => 'Product ID is missing'));
     }
 
     $product_id = intval($_POST['product_id']);
+    
+    // Start session if it's not started
+    if (!session_id()) {
+        session_start();
+    }
 
     $compare_list = isset($_SESSION['wpc_compare_list']) ? $_SESSION['wpc_compare_list'] : array();
+    
     if (!in_array($product_id, $compare_list)) {
         $compare_list[] = $product_id;
     }
+    
     $_SESSION['wpc_compare_list'] = $compare_list;
 
     wp_send_json_success(array('message' => 'Product added to compare list', 'compare_list' => $compare_list));
@@ -19,7 +26,6 @@ function wpc_add_to_compare() {
 
 add_action('wp_ajax_add_to_compare', 'wpc_add_to_compare');
 add_action('wp_ajax_nopriv_add_to_compare', 'wpc_add_to_compare');
-
 
 function wpc_remove_from_compare() {
     check_ajax_referer('wpc_compare_nonce', 'nonce');
